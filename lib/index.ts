970e6255ba71016bd08cb37c836b5d6da5e66f80
Josh Bowling <josh@balena.io>
2021-11-20 17:01:28 +0900

    Remove license headers
    Change-type: patch
    Signed-off-by: Josh Bowling <josh@balena.io>

--- lib/index.ts
+++ lib/index.ts
@@ -1,9 +1,3 @@
-/*
- * Copyright (C) Balena.io - All Rights Reserved
- * Unauthorized copying of this file, via any medium is strictly prohibited.
- * Proprietary and confidential.
- */
-
 import { defaultEnvironment, Mail } from '@balena/jellyfish-environment';
 import { Mailgun } from './mailgun';
 import { MailIntegration } from './types';
